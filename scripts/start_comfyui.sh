9a44310a57323028b1c5dd5730855b7cb8a7565f
dadada <1576401831@qq.com>
2026-05-25 14:46:24 +0800

    Update ComfyUI start script for new log path

--- scripts/start_comfyui.sh
+++ scripts/start_comfyui.sh
@@ -4,11 +4,11 @@ ARGS=("$@" --listen 0.0.0.0 --port 3001)
 
 export PYTHONUNBUFFERED=1
 echo "Starting ComfyUI"
-cd /workspace/ComfyUI
+cd /root/autodl-tmp/ComfyUI
 source venv/bin/activate
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
-python3 main.py "${ARGS[@]}" > /workspace/logs/comfyui.log 2>&1 &
+python3 main.py "${ARGS[@]}" > /root/autodl-tmp/logs/comfyui.log 2>&1 &
 echo "ComfyUI started"
-echo "Log file: /workspace/logs/comfyui.log"
+echo "Log file: /root/autodl-tmp/logs/comfyui.log"
 deactivate
